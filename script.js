fabb4d23c5eb886ca1c74c9f161d17138b92d489
najmull <nazmul.3@yahoo.com>
2021-01-01 15:30:08 +0600

    video updated

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ var imageMovingRightSite = document.querySelector(".image-slide .row.a .b");
 var upperUnderBackground = document.querySelector("#upperSite");
 var NameGetValue = document.querySelector(".takeName input");
 var resutlInputName = document.querySelector(".all-under .underBanner-text h3 span");
+var allUnder = document.querySelector(".all-under");
 var underVideo = document.querySelector(".all-under video");
 clickHereButton.addEventListener("click",function(){
     if(NameGetValue.value !==""){
         imageMovingLeftSite.classList.toggle("left-move");
         imageMovingRightSite.classList.toggle("right-move");
         upperUnderBackground.classList.toggle("uppersiteMove");
+        allUnder.style.zIndex = "100"
         resutlInputName.innerHTML = NameGetValue.value;
         underVideo.play();
         setTimeout(function(){
             imageMovingLeftSite.classList.remove("left-move");
             imageMovingRightSite.classList.remove("right-move");
             upperUnderBackground.classList.remove("uppersiteMove");
-        },30000)
+        allUnder.style.zIndex = "-5";
+
+        },45000)
 
     }
     else{
